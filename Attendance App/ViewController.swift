0fe40d632d7a043a44b4972ae69d777c1c678070
William-Yulio <william.yulio331@gmail.com>
2023-06-10 14:15:16 +0700

    Initial Commit

---
+++ Attendance App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Attendance App
+//
+//  Created by William Yulio on 10/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
